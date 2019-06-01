04d5e32c58c109a3b67cd0c31e16e0b6f9cd5712
Danio Zhu <zjh-517@163.com>
2019-06-01 08:12:54 +0800

    Update lock_free_stack.h

--- lock_free_stack.h
+++ lock_free_stack.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <atomic>
+#include <memory>
 
 template <typename T>
 class lock_free_stack
@@ -8,10 +9,10 @@ class lock_free_stack
 private:
     struct node
     {
-        T data;
+        std::shared_ptr<T> data;
         node* next;
         node(const T& _data)
-            :data(_data), next(nullptr)
+            :data(std::make_shared<T>(_data)), next(nullptr)
         {
         }
     };
@@ -26,9 +27,18 @@ public:
         new_node->next = head.load();
 
         // if head != new_node->next, (for example, head has been modified by other thread which also pushed an new node and changed head to point to that new node)
-        // set new_node->next to the modfied head (new_node->next = head.load()), and the function returns false, so the loop continues, now the head equals to the new_node->next,
-        //  so set head to the new_node (head = new_node.load()).
+        // set new_node->next to the modfied head (new_node->next = head), and the function returns false, so the loop continues, now the head equals to the new_node->next,
+        //  so set head to the new_node (head = new_node).
         while (!head.compare_exchange_weak(new_node->next, new_node));
     }
 
+    std::shared_ptr<T> pop()
+    {
+        node* old_head = head.load();
+        while (old_head && !head.compare_exchange_weak(old_head, old_head->next));
+
+        std::shared_ptr<T> data = old_head ? old_head->data : std::make_shared<T>();
+        delete old_head;
+        return data;
+    }
 };
